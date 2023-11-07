96baba6495998295599f87f5f50af24a2272d74b
kboucaud <kboucaud.pro@gmail.com>
2023-11-07 10:19:42 +0100

    contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,24 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const modalBackground = document.getElementById("modal-background");
+    modalBackground.style.display = "block";
 	modal.style.display = "block";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const modalBackground = document.getElementById("modal-background");
+    modalBackground.style.display = "none";
     modal.style.display = "none";
+}
+
+function submitForm(){
+    const firstName = document.getElementById("modal-first-name").value;
+    const lastName = document.getElementById("modal-last-name").value;
+    const email = document.getElementById("modal-email").value;
+    const message = document.getElementById("modal-message").value;
+
+    console.log(firstName, lastName, email, message);
+
+    return false;
 }
